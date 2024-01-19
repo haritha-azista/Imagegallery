5b16e70799dd767ccbb86df7646bbb09b30a95ad
haritha-azista <haritha.chintha@azistaindustries.com>
2024-01-19 10:47:50 +0530

    code

--- src/ImageGallery/ImageGallery.jsx
+++ src/ImageGallery/ImageGallery.jsx
@@ -1,39 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react';
+import { Button } from 'react-bootstrap';
 import './Image.css'
-import { Button } from 'react-bootstrap'
-import { useRef } from 'react';
+
 const ImageGallery = () => {
-
   const fileInputRef = useRef(null);
-  const [imageFile, setimageFile] = useState([]);
+  const [imageFiles, setImageFiles] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [imageNO, setImageNo] = useState(1);
-  var imageArray = [];
 
-  const handleUploadFiles = () => {
-    const files = fileInputRef.current.files;
-    console.log(files);
-    for (var i = 0; i < files.length; i++) {
-      imageArray.push(files[i]);
-    }
-    console.log(imageArray);
-    setimageFile(imageArray);
+  const handleUploadFiles = (event) => {
+    const files = event.target.files;
+    setImageFiles(Array.from(files));
+    setCurrentIndex(0);
   };
+
   const previousSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? imageArray.length - 1 : prevIndex - 1));
-    console.log(imageFile[currentIndex]);
-    setImageNo((currentIndex))
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? imageFiles.length - 1 : prevIndex - 1));
   };
 
   const nextSlide = () => {
-    setCurrentIndex((nextIndex) => (nextIndex === imageArray.length - 1 ? 0 : nextIndex + 1));
-    setImageNo(currentIndex + 2);
+    setCurrentIndex((nextIndex) => (nextIndex === imageFiles.length - 1 ? 0 : nextIndex + 1));
   };
 
-  const handleImageshow = (i) => {
-    setCurrentIndex(i);
-    setImageNo(i + 1);
-  }
+  const handleImageShow = (index) => {
+    setCurrentIndex(index);
+  };
 
   return (
     <div>
@@ -45,52 +35,69 @@ const ImageGallery = () => {
             webkitdirectory="true"
             onChange={handleUploadFiles}
             style={{ display: 'none' }}
+            multiple
           />
         </div>
-        <Button variant='info' size='sm' className='uploadButton' onClick={() => fileInputRef.current.click()}>Upload</Button>
+        <Button
+          variant='info'
+          size='sm'
+          className='uploadButton'
+          onClick={() => fileInputRef.current.click()}
+        >
+          Upload
+        </Button>
         <br />
         <div className='imageGalleryContainer'>
-
           <div className='imageListC'>
             <h6 className='ImgListHeading'>Images</h6>
-            {imageFile.map((File, index) => (
+            {imageFiles.map((file, index) => (
               <div key={index}>
-                <p className='imageList' style={{ fontSize: 'small' }} onClick={() => { handleImageshow(index) }}>{File.name}</p>
+                <p
+                  className='imageList'
+                  style={{ fontSize: 'small' }}
+                  onClick={() => handleImageShow(index)}
+                >
+                  {file.name}
+                </p>
               </div>
             ))}
           </div>
-
           <div className='ImageSlideContainer'>
-           
-            <Button as="input" type="button" value="<" size='sm' variant="secondary"
+            <Button
+              as="input"
+              type="button"
+              value="<"
+              size='sm'
+              variant="secondary"
               style={{ marginRight: '10px' }}
-              onClick={() => { previousSlide() }}
+              onClick={previousSlide}
             />
-
-            {imageFile.length > 0 && (
+            {imageFiles.length > 0 && (
               <img
-                src={`static/media/ImageFolder/${imageFile[currentIndex].webkitRelativePath}`}
-                alt={imageFile[currentIndex].name}
+                src={URL.createObjectURL(imageFiles[currentIndex])}
+                alt={imageFiles[currentIndex].name}
                 width='70%'
                 height='50%'
               />
             )}
-
-            <Button as="input" type="button" value=">" size='sm' variant="secondary"
+            <Button
+              as="input"
+              type="button"
+              value=">"
+              size='sm'
+              variant="secondary"
               style={{ marginLeft: '10px' }}
-              onClick={() => { nextSlide() }}
+              onClick={nextSlide}
             />
-
-            <p>{imageNO}/{imageFile.length}</p>
-
-            {imageFile.length > 0 && (
-              <p>{imageFile[currentIndex].name}</p>
+            <p>{currentIndex + 1}/{imageFiles.length}</p>
+            {imageFiles.length > 0 && (
+              <p>{imageFiles[currentIndex].name}</p>
             )}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ImageGallery
+export default ImageGallery;
